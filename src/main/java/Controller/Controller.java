3b3b4de78b3e32c9a77c8eb9ae76b2288430c40b
Upjaka <alesant-l-99@mail.ru>
2022-02-27 22:05:04 +0300

    Добавлены комментарии

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -49,10 +49,12 @@ public class Controller {
     @FXML
     private TextField heightTextField;
 
+    // Обработчик нажатия кнопки выхода из приложения
     public void exitButtonClicked() {
         Platform.exit();
     }
 
+    // Обработчик нажатия кнопки "Решить"
     public void startButtonClicked() throws IOException {
         if (isSizeCorrect()) {
             Pattern pattern = Pattern.compile("[^0-9]");
@@ -82,20 +84,27 @@ public class Controller {
         }
     }
 
+    // Обработчик нажатия кнопки возвращения с экрана с решений к экрану задания ввходных параметров
     public void newGameButtonClicked() {
         solution.close();
     }
 
+    // Возможность установить размер поля, если нажать Enter в одном из двух текстовых полей размера
     public void sizeChanged(KeyEvent keyEvent) {
         if (keyEvent.getCode() == KeyCode.ENTER) {
-            if (isSizeCorrect()) changeField();
+            setSizeButtonClicked();
         }
     }
 
+    // Обработчик нажатия кнопки установки заданного пользователем размера поля
     public void setSizeButtonClicked() {
-        if (isSizeCorrect()) changeField();
+        if (isSizeCorrect()) {
+            model.changeSize(Integer.parseInt(widthTextField.getText()), Integer.parseInt(heightTextField.getText()));
+            changeField();
+        }
     }
 
+    // Обработчик нажатия кнопки случайного размера поля
     public void randomSizeButtonClicked() {
         model.changeSize();
         widthTextField.setText(String.valueOf(model.getWidth()));
@@ -103,6 +112,7 @@ public class Controller {
         changeField();
     }
 
+    // Обработчик нажатия кнопки случайного содержимого поля
     public void randomContentButtonClicked() {
         if (gridPane.getColumnCount() != 0 && gridPane.getRowCount() != 0) {
             model.setRandomValues();
@@ -112,6 +122,7 @@ public class Controller {
         }
     }
 
+    // Обработчик нажатия кнопки "Показать решения"
     public void showButtonClicked() {
         model.solve();
         labels = new Label[model.getHeight()][model.getWidth()];
@@ -137,7 +148,11 @@ public class Controller {
 
         controlButtons.getChildren().remove(showButton);
 
-        if (model.getSolutionsSize() == 0) noSolutions();
+        if (model.getSolutionsSize() == 0) {
+            solutionsNumberLabel.setText("Нет решений");
+            nextButton.setVisible(false);
+            previousButton.setVisible(false);
+        }
         else {
             if (model.getSolutionsSize() != 1) {
                 previousButton.setVisible(true);
@@ -145,22 +160,27 @@ public class Controller {
                 nextButton.setVisible(true);
                 nextButton.getStyleClass().add("controlButton");
             }
+            labels[0][0].getStyleClass().add("borderCell");
+            labels[model.getHeight() - 1][model.getWidth() - 1].getStyleClass().add("borderCell");
             updateField();
         }
     }
 
+    // Обработчик нажатия кнопки показа следующего решения
     public void nextButtonClicked() {
         solutionNumber = solutionNumber == model.getSolutionsSize() ? 1 : solutionNumber + 1;
         solutionsNumberLabel.setText(solutionNumber + " из " + model.getSolutionsSize());
         updateField();
     }
 
+    // Обработчик нажатия кнопки показа предыдущего решения
     public void previousButtonClicked() {
         solutionNumber = solutionNumber == 1 ? model.getSolutionsSize() : solutionNumber - 1;
         solutionsNumberLabel.setText(solutionNumber + " из " + model.getSolutionsSize());
         updateField();
     }
 
+    // Изменяет отображамое поле в соответсвии с состоянием модели
     private void changeField() {
         if (gridPane.getColumnCount() == 0 && gridPane.getRowCount() == 0) {
             for (int i = 0; i < maxSize; i++) {
@@ -173,31 +193,28 @@ public class Controller {
         }
         gridPane.getChildren().clear();
 
-        int width = Integer.parseInt(widthTextField.getText());
-        int height = Integer.parseInt(heightTextField.getText());
-        model.changeSize(width, height);
-
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        for (int i = 0; i < model.getHeight(); i++) {
+            for (int j = 0; j < model.getWidth(); j++) {
                 textFields[i][j].setText(String.valueOf(model.get(i, j)));
                 gridPane.add(textFields[i][j], i, j);
             }
         }
 
-        while (gridPane.getColumnConstraints().size() < width) {
+        while (gridPane.getColumnConstraints().size() < model.getWidth()) {
             gridPane.getColumnConstraints().add(new ColumnConstraints());
         }
-        while (gridPane.getColumnConstraints().size() > width) {
+        while (gridPane.getColumnConstraints().size() > model.getWidth()) {
             gridPane.getColumnConstraints().remove(0);
         }
-        while (gridPane.getRowConstraints().size() < height) {
+        while (gridPane.getRowConstraints().size() < model.getHeight()) {
             gridPane.getRowConstraints().add(new RowConstraints());
         }
-        while (gridPane.getRowConstraints().size() > height) {
+        while (gridPane.getRowConstraints().size() > model.getHeight()) {
             gridPane.getRowConstraints().remove(0);
         }
     }
 
+    // Метод проверки корректности введеных размеров игрового поля
     private boolean isSizeCorrect() {
         if (widthTextField.getText().isEmpty() || heightTextField.getText().isEmpty()) {
             showAlert("Сначала установите размеры поля");
@@ -217,25 +234,19 @@ public class Controller {
         return true;
     }
 
-    private void noSolutions() {
-        solutionsNumberLabel.setText("Нет решений");
-        nextButton.setVisible(false);
-        previousButton.setVisible(false);
-    }
-
+    // Метод для ототбражения выбранного решения
     private void updateField() {
         for (int i = 0; i < model.getHeight(); i++) {
             for (int j = 0; j < model.getWidth(); j++) {
                 labels[i][j].getStyleClass().remove("solutionCell");
             }
         }
-        labels[0][0].getStyleClass().add("borderCell");
-        labels[model.getHeight() - 1][model.getWidth() - 1].getStyleClass().add("borderCell");
         for (Pair<Integer, Integer> pair : model.getSolutions().get(solutionNumber - 1)) {
             labels[pair.getKey()][pair.getValue()].getStyleClass().add("solutionCell");
         }
     }
 
+    // Создание всплывающего окна для информационных сообщений
     private void showAlert(String text) {
         Alert alert = new Alert(Alert.AlertType.NONE, text, ButtonType.OK);
         alert.setTitle("Внимание");
